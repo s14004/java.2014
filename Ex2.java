33fec0cd59f97a2ed8baf488882f6036b4d0d43d
yamauchi@std.it-college.ac.jp <s14004@std.it-college.ac.jp>
2014-10-28 11:07:10 +0900

    Ex2

--- Ex2.java
+++ Ex2.java
@@ -51,38 +51,59 @@ class Ex2_2 extends Ex{
          System.out.println("平均は" + (x + y + z) / 3 + "です。"); 
      }
 }
-  
 
+class Ex2_3 extends Ex{ 
+     void execute(){ 
+       Scanner stdIn = new Scanner(System.in);
 
-class Ex2_00 extends Ex{
-	void execute(){
-		Scanner stdIn = new Scanner(System.in);
+       System.out.print("整数値: ");
+       int a = stdIn.nextInt();
 
-		System.out.println("xとyを加減乗除します。");
-
-		System.out.print("xの値：");		// xの値の入力を促す
-		int x = stdIn.nextInt();
-		
-		System.out.print("yの値：");		// yの値の入力を促す
-		int y = stdIn.nextInt();
-
-		System.out.println("x + x = " + (x + y)); // x + yの値を表示
-		System.out.println("x - x = " + (x - y)); // x - yの値を表示
-		System.out.println("x * x = " + (x * y)); // x * yの値を表示
-		System.out.println("x / x = " + (x / y)); // x / yの値を表示 (商)
-		System.out.println("x % x = " + (x % y)); // x % yの値を表示 (剰余)
-	}
+       System.out.println(a + "です。");
+     }
 }
 
-class Ex2_000 extends Ex{
-	void execute(){
-		Random rand = new Random();
+class Ex2_4 extends Ex{ 
+     void execute(){
+       Scanner stdIn = new Scanner(System.in);
 
-		int luchy = rand.nextInt(10);		// 0～9の乱数
+       System.out.print("整数値: ");
+       int a = stdIn.nextInt();
 
-		System.out.println("今日のラッキーナンバーは" + luchy + "です。");
-	}
+       System.out.println((a + 10) + "です。");
+       System.out.println((a - 10) + "です。");
+     }
 }
+
+class Ex2_5 extends Ex{ 
+     void execute(){
+       Scanner stdIn = new Scanner(System.in);
+
+       System.out.print("xの値: ");
+       double x = stdIn.nextDouble();
+
+       System.out.print("yの値: ");
+       double y = stdIn.nextDouble();
+
+       System.out.println("合計は" + (x + y) + "です。");
+       System.out.println("平均は" + (x + y) / 2 + "です。");
+     }
+}
+
+class Ex2_6 extends Ex{ 
+     void execute(){
+       Scanner stdIn = new Scanner(System.in);
+
+       System.out.print("底辺: ");
+       double x = stdIn.nextDouble();
+
+       System.out.print("高さ: ");
+       double y = stdIn.nextDouble();
+
+       System.out.println("三角形の面積は" + (x * y) / 2 + "です。");
+     }
+}
+
 
 class Ex2_00 extends Ex{
 	void execute(){
@@ -128,11 +149,15 @@ public class Ex2 {
     a = new Ex2_1();
     a.execute();
     a = new Ex2_2();
-=======
-*/
-		a = new Ex2_000();
->>>>>>> 787164609cb6ea6bdd9cef349a592f7d73a7d607
-		a.execute();
+    a.execute();
+    a = new Ex2_3();
+    a.execute();
+    a = new Ex2_4();
+    a.execute();
+    a = new Ex2_5();
+    a.execute();
+    a = new Ex2_6();
+    a.execute();
     
 	}
 }
